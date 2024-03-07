2ddf3a335a37d2d912ffd8ece7e835b8a9f24309
Rahul Ganesh Sawant <rahulsawant8433@gmail.com>
2024-03-07 22:53:26 +0000

    Add project files.

---
+++ DU_test/Model/JobOffer.cs
@@ -0,0 +1,9 @@
+﻿namespace DU_test.Model
+{
+    public class JobOffer
+    {
+        public string JobOfferId { get; set; } 
+        public DateTime datetime { get; set;}
+        public string description { get; set;}
+    }
+}
